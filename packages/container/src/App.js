7ce420287a4e9410414740add24f23a6305cf9e3
Sanchit Bhatnagar <sanbht23@gmail.com>
2021-07-07 18:53:46 +0530

    Added custom css prefix, added Header Component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {BrowserRouter} from "react-router-dom";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 const App = () => {
   return (
-      <React.Fragment>
-        <h1>Hi There!!!!!</h1>
-        <hr/>
-        <MarketingApp/>
-      </React.Fragment>
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassname}>
+          <React.Fragment>
+            <Header/>
+            <MarketingApp/>
+          </React.Fragment>
+        </StylesProvider>
+      </BrowserRouter>
   );
 };
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {StylesProvider} from "@material-ui/core";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
+
+const generateClassname = createGenerateClassName({
+  productionPrefix: 'ma'
+})
 
 const App = () => {
   return (
       <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassname}>
           <BrowserRouter>
             <Switch>
               <Route exact path="/pricing" component={Pricing}/>
